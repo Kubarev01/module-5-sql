9acb763da2a5b0aaba127fe30d710833df3ea2bd
Egor Kubarev <kubarevegor@yandex.ru>
2025-10-29 13:00:53 +0300

    realations added

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column,relationship
 
 
 
@@ -14,11 +13,12 @@ class Book(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String, nullable=False)
     genre : Mapped[str] = mapped_column(String, nullable=False)
-    author_id: Mapped[int]
+    author_id : Mapped[int] = mapped_column(Integer, sqlalchemy.ForeignKey('authors.id'), nullable=False)
 
 class Author(Base):
     __tablename__ = 'authors'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
+   
     
