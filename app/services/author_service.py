86fa76f8646db3decd617d6fa07f161a7d50c712
Egor Kubarev <kubarevegor@yandex.ru>
2025-11-04 14:13:32 +0300

    timeout for get author details

--- app/services/author_service.py
+++ app/services/author_service.py
@@ -1,17 +1,22 @@
 
 from httpx import AsyncClient
-
+import asyncio
 class AuthorService:
     def __init__(self, repo, base_url: str):
         self.repo = repo
         self.client = AsyncClient(base_url=base_url)
 
-    async def get_authors(self):
-        # Пример метода для получения авторов
-        response = await self.client.get("/authors")
-        response.raise_for_status()
-        return response.json()
+   
+    async def get_author_details(self, author_id: int):
+        try:
+            author = await asyncio.wait_for( self.repo.get_by_id(author_id), timeout=2.0)
+        except asyncio.TimeoutError:
+            print("Request timed out")
+            return None
+            
 
-    async def close(self):
-        # Закрываем клиент, чтобы освободить соединения
-        await self.client.aclose()
+        response = await self.client.get(f"/authors/{author_id}/details")
+        if response.status_code == 200:
+            details = response.json()
+            author.details = details
+        return author
