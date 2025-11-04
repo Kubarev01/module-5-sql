164a2155a8618b87d36b359e1123c1933d1b890b
Egor Kubarev <kubarevegor@yandex.ru>
2025-11-04 12:18:06 +0300

    added async create book test

--- app/tests/test_fastapi_integrations.py
+++ app/tests/test_fastapi_integrations.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 from main import app 
 from database.postgres_client import get_db_session
-
+import pytest
+from httpx import AsyncClient
+from httpx._transports.asgi import ASGITransport
 
 book_id = 35
 class FakeDbSession:
@@ -28,3 +30,21 @@ def test_get_books():
     response = client.get(f"/books/{book_id}",)  
     assert response.status_code == 200 
     app.dependency_overrides.pop(get_db_session, None)
+
+
+
+@pytest.mark.asyncio
+async def test_create_book():
+    app.dependency_overrides[get_db_session] = fake_get_db_session
+    
+
+    new_book_data = {
+        "title": "New Book",
+        "genre": "Fiction",
+        "author_id": 1
+    }
+    transport = ASGITransport(app=app) # обернул приложение в транспорт
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post("/books/", json=new_book_data)  
+        assert response.status_code == 201  
+        app.dependency_overrides.pop(get_db_session, None)
