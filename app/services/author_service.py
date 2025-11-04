efe1a11e38e6673da613404ab9c1a21b382ad000
Egor Kubarev <kubarevegor@yandex.ru>
2025-11-04 19:06:49 +0300

    simple breaker added

--- app/services/author_service.py
+++ app/services/author_service.py
@@ -2,21 +2,23 @@
 from httpx import AsyncClient, RequestError
 import asyncio
 import backoff
+import pybreaker
 
 class AuthorService:
     def __init__(self, repo, base_url: str):
         self.repo = repo
-        self.client = AsyncClient(base_url=base_url)
+        self.client = AsyncClient(base_url = base_url)
+        self.breaker = pybreaker.CircuitBreaker(fail_max = 5)
 
-    @backoff.on_exception(backoff.expo, RequestError, max_tries=3)
+    @backoff.on_exception(backoff.expo, RequestError, max_tries = 3)
     async def get_author_details(self, author_id: int):
         try:
-            author = await asyncio.wait_for(self.repo.get_by_id(author_id), timeout=2.0)
+            author = await asyncio.wait_for(self.repo.get_by_id(author_id), timeout = 2.0)
         except asyncio.TimeoutError:
             print("Request timed out")
             return None
             
-        response = await self.client.get(f"/authors/{author_id}/details")
+        response = await self.breaker.call_async(self.client.get(f"/authors/{author_id}"))
         if response.status_code == 200:
             details = response.json()
             author.details = details
