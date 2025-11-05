d2a41295ebd9796dced9e8f2a70170fb43e5c78a
Egor Kubarev <kubarevegor@yandex.ru>
2025-11-05 13:01:24 +0300

    fallback added to author_service

--- app/services/author_service.py
+++ app/services/author_service.py
@@ -1,16 +1,16 @@
-from httpx import AsyncClient, RequestError
+from httpx import AsyncClient, RequestError, ReadTimeout
 import asyncio
 import backoff
 from types import SimpleNamespace
-from datetime import timedelta
 
 from aiobreaker import CircuitBreaker, CircuitBreakerError
 
 
 class DummyRepo:
     async def get_by_id(self, author_id: int):
-        return SimpleNamespace(details=None)
-
+        # просто заглушка, чтобы не мешала логике breaker’а
+        return SimpleNamespace(id=author_id, name=None)
+    
 
 class AuthorService:
     def __init__(self, repo, base_url="http://testserver"):
@@ -19,19 +19,29 @@ class AuthorService:
 
         self.breaker = CircuitBreaker(
             fail_max=5,
-           
+        
         )
 
-    @backoff.on_exception(backoff.expo, RequestError, max_tries=1)
+    @backoff.on_exception(backoff.expo, (RequestError, ReadTimeout), max_tries=3)
     async def get_author_details(self, author_id: int):
         try:
             author = await asyncio.wait_for(self.repo.get_by_id(author_id), timeout=2.0)
         except asyncio.TimeoutError:
-            print("Request timed out")
+            print("Repo timed out")
             return None
 
+        try:
+            response = await self.breaker.call_async(
+                self.client.get,
+                f"/authors/{author_id}",
+            )
+        except CircuitBreakerError:
 
-        response = await self.breaker.call_async(self.client.get, f"/authors/{author_id}")
+            return {
+                "id": author_id,
+                "name": "Default Author",
+            }
+
 
         if response.status_code == 200:
             return response.json()
